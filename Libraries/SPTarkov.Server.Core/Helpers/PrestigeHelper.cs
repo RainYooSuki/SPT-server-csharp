1dd688e477da81374c437e81ae4b0607baa9ec79
Archangel <jesse@archangel.wtf>
2025-12-27 20:38:32 +0100

    Clear dropped items

--- Libraries/SPTarkov.Server.Core/Helpers/PrestigeHelper.cs
+++ Libraries/SPTarkov.Server.Core/Helpers/PrestigeHelper.cs
@@ -156,6 +156,7 @@ public class PrestigeHelper(
         newPmcStats.Eft.CarriedQuestItems = [];
         newPmcStats.Eft.FoundInRaidItems = [];
         newPmcStats.Eft.LastSessionDate = 0;
+        newPmcStats.Eft.DroppedItems = [];
 
         // TODO: find evidence scav stats are copied over in live
         //var newScavStats = newProfile.CharacterData.ScavData.Stats;
